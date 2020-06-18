4077b1630f8b6f24c88c77e3a789dc8352e9e85e
Frank Pinnola <frankie4pinnola@gmail.com>
2020-06-17 21:36:51 -0400

    Testing without redis-server

--- app.js
+++ app.js
@@ -1,88 +1,39 @@
-const { MongoClient } = require("mongodb");
-var kue = {};
+const express = require("express"),
+  app = express(),
+  bodyParser = require("body-parser");
+const kue = require("./kue");
 require("./worker");
 
-const RedisServer = require("redis-server");
+// support parsing of application/json type post data
+app.use(bodyParser.json());
 
-const server = new RedisServer(6379);
-console.log("Hello world");
+app.post("/book-ticket", async (req, res) => {
+  let args = {
+    jobName: "sendEmail",
+    time: 1000,
+    params: {
+      email: req.body.email,
+      subject: "Booking Confirmed",
+      body: "Your booking is confirmed!!",
+    },
+  };
+  kue.scheduleJob(args);
 
-server.open((err) => {
-  if (err === null) {
-    kue = require("./kue");
-    main().catch(console.error);
-  } else {
-    console.log("Error: " + err);
-  }
+  // Schedule Job to send email 10 minutes before the movie
+  args = {
+    jobName: "sendEmail",
+    time: (req.body.start_time - 10 * 60) * 1000, // (Start time - 10 minutes) in millieconds
+    params: {
+      email: req.body.email,
+      subject: "Movie starts in 10 minutes",
+      body:
+        "Your movie will start in 10 minutes. Hurry up and grab your snacks.",
+    },
+  };
+  kue.scheduleJob(args);
+
+  // Return a response
+  return res.status(200).json({ response: "Booking Successful!" });
 });
 
-async function main() {
-  const uri =
-    "mongodb+srv://fpinnola:F55server-e@cluster0-w3lve.mongodb.net/NotificationQueue?retryWrites=true&w=majority";
-  const client = new MongoClient(uri, { useUnifiedTopology: true });
-  try {
-    await client.connect();
-
-    const collection = client.db("NotificationQueue").collection("Data");
-    const changeStream = collection.watch(
-      [{ $match: { operationType: "insert" } }],
-      { fullDocument: "updateLookup" }
-    );
-    changeStream.on("change", (next) => {
-      //UPDATE switch token to array, for sending same notif to group of users
-      let args = {
-        jobName: "sendNotification",
-        time: next.fullDocument.time * 1000,
-        params: {
-          token: next.fullDocument.token,
-          title: next.fullDocument.title,
-          body: next.fullDocument.body,
-        },
-      };
-      kue.scheduleJob(args);
-    });
-  } catch (e) {
-    console.log(e);
-  }
-}
-
-// async function getAllNotifications(client) {
-//   const cursor = client.db("NotificationQueue").collection("Data").find();
-//   const results = await cursor.toArray();
-//   if (results.length > 0) {
-//     console.log(results);
-//   } else {
-//     console.log("Found Nun");
-//   }
-// }
-
-// async function observe(client) {
-//   cursor = client
-//     .db("NotificationQueue")
-//     .collection("Data")
-//     .changes([
-//       {
-//         $match: {
-//           operationType: { $in: ["insert", "replace"] },
-//         },
-//       },
-//       {
-//         $match: {
-//           "newDocument.n": { $gte: 1 },
-//         },
-//       },
-//     ]);
-//   const results = await cursor.toArray();
-
-//   // # Loops forever.
-//   for (var i = 0; i < cursor.length; i++) {
-//     print(change[i]["newDocument"]);
-//   }
-// }
-
-// async function listDatabases(client) {
-//   databasesList = await client.db().admin().listDatabases();
-
-//   console.log("Databases:");
-//   databasesList.databases.forEach((db) => console.log(` - ${db.name}`));
-// }
+app.listen(8080, () => console.log(`Hey there! I'm listening.`));
