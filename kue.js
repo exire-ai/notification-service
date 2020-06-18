1e0c03c08c1c790b17b37e2a4ce002d7e9f806d6
Hayden Daly <hdaly1@stevens.edu>
2020-06-17 22:03:39 -0700

    added proc

--- kue.js
+++ kue.js
@@ -1,8 +1,8 @@
 var kue = require("kue");
 
 var Queue = kue.createQueue({ redis: {
-  port: process.env.REDIS_PORT,
-  host: process.env.REDIS_HOST
+  port: process.env.REDIS_PORT || process.env.REDIS_PORT,
+  host: process.env.REDIS_HOST || 'redis//127.0.0.1'
 }});
 
 let scheduleJob = (data) => {
