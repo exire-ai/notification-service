4f00468c3029011c03262eab8f2521b05ba54940
Hayden Daly <hdaly1@stevens.edu>
2020-06-17 21:47:40 -0700

    working?

--- kue.js
+++ kue.js
@@ -1,10 +1,6 @@
 var kue = require("kue");
 
-if (process.env.REDISTOGO_URL) {
-  var Queue = kue.createQueue({ redis: process.env.REDISTOGO_URL });
-} else {
-  var Queue = kue.createQueue()
-}
+var Queue = kue.createQueue({ redis: process.env.REDIS_URL || 'redis://127.0.0.1:6379' });
 
 let scheduleJob = (data) => {
   Queue.createJob(data.jobName, data.params)
