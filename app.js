732af3345c123d52618d87f70264dd26101fb783
Frank Pinnola <frankie4pinnola@gmail.com>
2020-06-25 22:44:49 -0400

    Allow CORS

--- app.js
+++ app.js
@@ -10,42 +10,51 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-main().catch(console.error);
+// main().catch(console.error);
 
 app.set("port", port);
 
-async function main() {
-  const uri =
-    "mongodb+srv://fpinnola:F55server-e@cluster0-w3lve.mongodb.net/NotificationQueue?retryWrites=true&w=majority";
-  const client = new MongoClient(uri, { useUnifiedTopology: true });
-  try {
-    await client.connect();
+// async function main() {
+//   const uri =
+//     "mongodb+srv://fpinnola:F55server-e@cluster0-w3lve.mongodb.net/NotificationQueue?retryWrites=true&w=majority";
+//   const client = new MongoClient(uri, { useUnifiedTopology: true });
+//   try {
+//     await client.connect();
 
-    const collection = client.db("NotificationQueue").collection("Data");
-    const changeStream = collection.watch(
-      [{ $match: { operationType: "insert" } }],
-      { fullDocument: "updateLookup" }
-    );
-    changeStream.on("change", (next) => {
-      //UPDATE switch token to array, for sending same notif to group of users
-      let args = {
-        jobName: "sendNotification",
-        time: next.fullDocument.time * 1000,
-        params: {
-          token: next.fullDocument.token,
-          title: next.fullDocument.title,
-          body: next.fullDocument.body,
-        },
-      };
-      kue.scheduleJob(args);
-    });
-  } catch (e) {
-    console.log(e);
-  }
-}
+//     const collection = client.db("NotificationQueue").collection("Data");
+//     const changeStream = collection.watch(
+//       [{ $match: { operationType: "insert" } }],
+//       { fullDocument: "updateLookup" }
+//     );
+//     changeStream.on("change", (next) => {
+//       //UPDATE switch token to array, for sending same notif to group of users
+//       let args = {
+//         jobName: "sendNotification",
+//         time: next.fullDocument.time * 1000,
+//         params: {
+//           token: next.fullDocument.token,
+//           title: next.fullDocument.title,
+//           body: next.fullDocument.body,
+//         },
+//       };
+//       kue.scheduleJob(args);
+//     });
+//   } catch (e) {
+//     console.log(e);
+//   }
+// }
 
 app.get("/", (req, res) => res.send("Hello World!"));
 app.post("/add", (req, res) => {
